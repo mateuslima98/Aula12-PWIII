cdfef5871f36c03bc4320283defa49e779f20186
MateuseBia <mateus_lima98@hotmail.com>
2019-10-11 22:23:07 -0300

    PWIII- Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import{ProdutosComponent} from './produtos/produtos.component'
+import{NovoProdutoComponent} from './novo-produto/novo-produto.component'
+import{ContentComponent} from './content/content.component'
 
-const routes: Routes = [];
+  //Vetor
+const routes: Routes = [
+
+{
+  path:'produtos',
+  component:ProdutosComponent
+},
+{
+  path:'novo-produto',
+  component:NovoProdutoComponent
+},
+{
+  path:'content',
+  component:ContentComponent
+},
+{
+  path:'',
+  component:ContentComponent
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
